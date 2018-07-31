6d52616f63ca0c22fa5bc63d71985434e781a708
Atila Romero <atilaromero@gmail.com>
2018-07-31 16:02:33 -0300

    minor fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 (cd dependencies; docker build . -t ipeddocker/dependencies)
-(cd runtime; docker build . -t ipeddocker/runtime)
+(cd precompiled; docker build . -t ipeddocker/precompiled)
 (cd build; docker build . -t ipeddocker/build)
