10557218c367d8d770d262e11b5e4886b7160a3e
Atila Romero <atilaromero@gmail.com>
2018-12-06 16:32:12 -0200

    3.14.5

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 (cd dependencies; docker build . -t ipeddocker/dependencies)
-(cd precompiled; docker build . -t ipeddocker/precompiled)
+(cd precompiled; docker build . -t ipeddocker/iped)
 (cd build; docker build . -t ipeddocker/build)
