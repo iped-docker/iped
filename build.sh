b1389eca6a8758d3b60a7d88857cae95afd043c9
Atila Romero <atilaromero@gmail.com>
2019-01-30 14:42:28 -0200

    removing obfuscation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 (cd dependencies; docker build . -t ipeddocker/dependencies)
-(cd ../iped/precompiled; docker build . -t 192.168.2.191:5001/ipeddocker/iped:3.15)
+(cd precompiled; docker build . -t 192.168.2.191:5001/ipeddocker/iped:3.15.1)
