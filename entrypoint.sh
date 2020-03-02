368a5e40816675c8d7a2721f6808bf243b98ecb4
arisjr <aristeu.jr@gmail.com>
2020-03-02 17:28:08 -0300

    PhontoDNA check, LED vs KFF flags and IPEDConfig.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 PHOTODNA=false
 LED=false
 KFF=false
-if [ -z "$(ls /mnt/PhotoDNA)" ]
+if [ -z "$(ls /mnt/PhotoDNA)" ] && [ -z "$(ls /root/IPED/optional_jars/photodna-*.jar)" ] 
 then
         PHOTODNA=true
 fi
@@ -17,13 +17,23 @@ then
         KFF=true
 fi
 
-sed -i -e "s/enablePhotoDNA =.*/enablePhotoDNA = $PHOTODNA/" /root/IPED/iped/IPEDConfig.txt
+#
+# Note: Changes in the root IPEDConfig.txt are avoided in the new IPED Version
+# when the locale variable is defined.
+# 
+# Setting PhotoDNA related flags
+# sed -i -e "s/enablePhotoDNA =.*/enablePhotoDNA = $PHOTODNA/" /root/IPED/iped/IPEDConfig.txt
 sed -i -e "s/enablePhotoDNA =.*/enablePhotoDNA = $PHOTODNA/" /root/IPED/iped/profiles/*/default/IPEDConfig.txt
 
-sed -i -e "s/enableLedDie =.*/enableLedDie = $LED/" /root/IPED/iped/IPEDConfig.txt
+# Setting LED related flags
+# sed -i -e "s/enableLedDie =.*/enableLedDie = $LED/" /root/IPED/iped/IPEDConfig.txt
 sed -i -e "s/enableLedDie =.*/enableLedDie = $LED/" /root/ILED/iped/profiles/*/default/IPEDConfig.txt
+# sed -i -e "s/enableLedWkff =.*/enableLedWkff = $LED/" /root/IPED/iped/IPEDConfig.txt
+sed -i -e "s/enableLedWkff =.*/enableLedWkff = $LED/" /root/IPED/iped/profiles/*/default/IPEDConfig.txt
 
-sed -i -e "s/enableLedWkff =.*/enableLedWkff = $KFF/" /root/IPED/iped/IPEDConfig.txt
-sed -i -e "s/enableLedWkff =.*/enableLedWkff = $KFF/" /root/IPED/iped/profiles/*/default/IPEDConfig.txt
+# Setting KFF related flags
+# sed -i -e "s/enableKff =.*/enableKff = $KFF/" /root/IPED/iped/IPEDConfig.txt
+sed -i -e "s/enableKff =.*/enableKff = $KFF/" /root/IPED/iped/profiles/*/default/IPEDConfig.txt
+
 
 exec "$@"
