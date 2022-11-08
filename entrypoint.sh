aec9ca2a6a655eddc7845d016d4deef4a58cb58c
Aristeu Jr <aristeu.jr@gmail.com>
2022-11-08 16:05:24 -0300

    entrypoint and dockerfile updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ then
         then
                 echo -n "Copying iped-hashes.db to /mnt/ipedtmp..." && \
                 cp --update /mnt/hashesdb/iped-hashes.db /mnt/ipedtmp/ && echo -n OK... && \
-                echo "Updating config files ..." && \
+                echo -n "Updating config..." && \
                 sed -i -e "s/hashesDB =.*/hashesDB = \/mnt\/ipedtmp\/iped-hashes.db/" /root/IPED/iped/LocalConfig.txt && \
                 echo OK. || -n echo Failed.
 
